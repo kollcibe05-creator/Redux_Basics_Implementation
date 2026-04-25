1305333955bbe91cffcb90443820fa4e6700fd17
Collins Kibet <kollcibe05@gmail.com>
2026-04-25 17:58:19 +0300

    added todoReducer

--- store.js
+++ store.js
@@ -1,10 +1,11 @@
 import { configureStore } from "@reduxjs/toolkit";
 import movieReducer from  "./movieSlice"
-
+import todoReducer from "./todoSlice"
 
 
 export const store = configureStore({
      reducer: {    //takes two props ~ a state and an action
         movies: movieReducer,
+        todos: todoReducer
      }
 })  
